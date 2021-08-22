9d028e6558a8a77d7bacc04f4e955195b008bfa4
Gabriel Senna <msgabrielrafael@gmail.com>
2021-08-22 07:59:20 -0300

    Simplificando atribuição de valores para às classes Player, State e Attribute

--- index.php
+++ index.php
@@ -9,43 +9,52 @@ use App\Career\Attribute;
 use App\Career\State;
 use App\Career\History;
 
-$player = new Player();
-$player->name = 'Vinicius';
-$player->lastname = 'Coutinho';
-$player->nacionality = 'Brazil';
-$player->uniformName = 'COUTINHO';
-$player->uniformNumber = 9;
-$player->birthYear = 1997;
-$player->birthMonth = 10;
-$player->birthDay = 16;
+try
+{
+    $player = new Player([
+        "name"          => 'Vinicius',
+        "lastname"      => 'Coutinho',
+        "nacionality"   => 'Brazil',
+        "uniformName"   => 'COUTINHO',
+        "uniformNumber" => 9,
+        "birthYear"     => 1997,
+        "birthMonth"    => 10,
+        "birthDay"      => 16
+    ]);
+    
+    $player->setAttributes([
+        "height"      => 197,
+        "weight"      => 70,
+        "position"    => 'ATA',
+        "function"    => 'ATA',
+        "styleOfGame" => 'Creator',
+        "goodLeg"     => 'Rigth'
+    ]);
 
-$attribute = new Attribute();
-$attribute->height = 197;
-$attribute->weight = 70;
-$attribute->position = 'ATA';
-$attribute->function = 'ATA';
-$attribute->styleOfGame = 'Creator';
-$attribute->goodLeg = 'Rigth';
+    $player->setState([
+        "games"      => 186,
+        "goals"      => 193,
+        "assists"    => 90,
+        "bruise"     => false,
+        "suspencion" => false
+    ]);
+    
+    $history = new History();
 
-$state = new State;
-$state->games = 186;
-$state->goals = 193;
-$state->assists = 90;
-$state->bruise = false;
-$state->suspencion = false;
-
-$player->setAttributes($attribute);
-$player->setState($state);
-
-$history = new History();
-$history->timeLine($player, 'Real Madrid', '2020/2021');
-$history->timeLine($player, 'Real Madrid', '2021/2022');
-$history->timeLine($player, 'Minnesota United', '2021/2022');
-$history->timeLine($player, 'Fiorentina', '2022/2023');
-$history->timeLine($player, 'Fiorentina', '2024/2025');
-$history->timeLine($player, 'Juventos', '2025/2026');
-$history->timeLine($player, 'Real Madrid', '2025/2026');
-$history->timeLine($player, 'FC Bayern', '2025/2026');
-$history->timeLine($player, 'Chelsea', '2026/2027');
-echo '<pre>';
-dump($player, $history->getTimeLine());
+    $history->timeLine($player, 'Real Madrid', '2020/2021');
+    $history->timeLine($player, 'Real Madrid', '2021/2022');
+    $history->timeLine($player, 'Minnesota United', '2021/2022');
+    $history->timeLine($player, 'Fiorentina', '2022/2023');
+    $history->timeLine($player, 'Fiorentina', '2024/2025');
+    $history->timeLine($player, 'Juventos', '2025/2026');
+    $history->timeLine($player, 'Real Madrid', '2025/2026');
+    $history->timeLine($player, 'FC Bayern', '2025/2026');
+    $history->timeLine($player, 'Chelsea', '2026/2027');
+    
+    echo '<pre>';
+    dump($player, $history->getTimeLine());
+}
+catch (Exception $e)
+{
+    echo $e->getMessage();
+} 
